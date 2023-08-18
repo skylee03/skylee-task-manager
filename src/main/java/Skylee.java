a1f2510dac22f0a04b8d5dd2d5e927352c38f0d7
skylee03 <1178715749@qq.com>
2023-08-18 17:48:26 +0800

    Add greeting message

--- src/main/java/Skylee.java
+++ src/main/java/Skylee.java
@@ -1,10 +1,11 @@
 public class Skylee {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "____________________________________________________________\n" +
+                " Hello! I'm Skylee!\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
     }
 }
