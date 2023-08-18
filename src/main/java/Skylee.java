774db48572a5755342dce512fcc703a569769bf0
skylee03 <1178715749@qq.com>
2023-08-18 17:47:16 +0800

    Rename Duke to Skylee

--- src/main/java/Skylee.java
+++ src/main/java/Skylee.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Skylee {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
