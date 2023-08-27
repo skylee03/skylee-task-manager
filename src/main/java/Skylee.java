e6748dcc5bf48ff67ae28d61fc6d18f5b7cf4af5
skylee03 <1178715749@qq.com>
2023-08-27 13:28:53 +0800

    Add echo

--- src/main/java/Skylee.java
+++ src/main/java/Skylee.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Skylee {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
+        String helloMsg = "____________________________________________________________\n" +
                 " Hello! I'm Skylee!\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        String byeMsg = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(helloMsg);
+        for (String command;;) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(byeMsg);
+                break;
+            } else {
+                String msg = "____________________________________________________________\n" +
+                        " " + command + "\n" +
+                        "____________________________________________________________\n";
+                System.out.println(msg);
+            }
+        }
     }
 }
