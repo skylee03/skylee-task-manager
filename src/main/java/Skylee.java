31b4882cb7c18c76664be8b60f12e350e02af4c2
skylee03 <1178715749@qq.com>
2023-09-01 19:41:10 +0800

    Decompose Skylee.main to avoid long method

--- src/main/java/Skylee.java
+++ src/main/java/Skylee.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Skylee {
     private static final String LINE = "____________________________________________________________\n";
+    private static final String HELLO_MESSAGE = LINE + " Hello! I'm Skylee!\n What can I do for you?\n" + LINE;
+    private static final String BYE_MESSAGE = LINE + " Bye. Hope to see you again soon!\n" + LINE;
     private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
 
-    private static void addTask(Task task) {
+    private static void addTaskAndPrint(Task task) {
         tasks[taskCount] = task;
         taskCount++;
         System.out.print(LINE);
@@ -15,61 +17,64 @@ public class Skylee {
         System.out.println(LINE);
     }
 
+    private static void markAndPrint(Task task) {
+        task.markAsDone();
+        System.out.print(LINE);
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println("   " + task);
+        System.out.println(LINE);
+    }
+
+    private static void unmarkAndPrint(Task task) {
+        task.unmarkAsNotDone();
+        System.out.print(LINE);
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println("   " + task);
+        System.out.println(LINE);
+    }
+
+    private static void printTasks() {
+        System.out.print(LINE);
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(" " + (i + 1) + "." + tasks[i]);
+        }
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
-        String helloMsg = LINE +
-                " Hello! I'm Skylee!\n" +
-                " What can I do for you?\n" +
-                LINE;
-        String byeMsg = LINE +
-                " Bye. Hope to see you again soon!\n" +
-                LINE;
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println(helloMsg);
+        System.out.println(HELLO_MESSAGE);
         for (String command;;) {
             command = scanner.nextLine();
             if (command.equals("bye")) {
-                System.out.println(byeMsg);
+                System.out.println(BYE_MESSAGE);
                 break;
             } else if (command.equals("list")) {
-                System.out.print(LINE);
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks[i]);
-                }
-                System.out.println(LINE);
+                printTasks();
             } else if (command.startsWith("mark ")) {
                 int i = Integer.parseInt(command.substring("mark ".length())) - 1;
-                tasks[i].markAsDone();
-                System.out.print(LINE);
-                System.out.println(" Nice! I've marked this task as done:");
-                System.out.println("   " + tasks[i]);
-                System.out.println(LINE);
+                markAndPrint(tasks[i]);
             } else if (command.startsWith("unmark ")) {
                 int i = Integer.parseInt(command.substring("unmark ".length())) - 1;
-                tasks[i].unmarkAsNotDone();
-                System.out.print(LINE);
-                System.out.println(" OK, I've marked this task as not done yet:");
-                System.out.println("   " + tasks[i]);
-                System.out.println(LINE);
+                unmarkAndPrint(tasks[i]);
             } else if (command.startsWith("todo ")) {
                 String description = command.substring("todo ".length());
-                addTask(new Todo(description));
+                addTaskAndPrint(new Todo(description));
             } else if (command.startsWith("deadline ")) {
                 int byIndex = command.indexOf(" /by ");
                 String description = command.substring("deadline ".length(), byIndex);
                 String by = command.substring(byIndex + " /by ".length());
-                addTask(new Deadline(description, by));
+                addTaskAndPrint(new Deadline(description, by));
             } else if (command.startsWith("event ")) {
                 int fromIndex = command.indexOf(" /from ");
                 int toIndex = command.indexOf(" /to ");
                 String description = command.substring("event ".length(), fromIndex);
                 String from = command.substring(fromIndex + " /from ".length(), toIndex);
                 String to = command.substring(toIndex + " /to ".length());
-                addTask(new Event(description, from, to));
+                addTaskAndPrint(new Event(description, from, to));
             } else {
-                addTask(new Task(command));
+                addTaskAndPrint(new Task(command));
             }
         }
     }
