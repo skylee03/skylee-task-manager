1b0b910e883176b3ddd15220227d214ee71eabda
skylee03 <1178715749@qq.com>
2023-09-27 00:38:43 +0800

    Update test-ui-test

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\skylee\*.java ..\src\main\java\skylee\task\*.java ..\src\main\java\skylee\io\*.java  ..\src\main\java\skylee\exception\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\skylee\*.java ..\src\main\java\skylee\task\*.java ..\src\main\java\skylee\ui\*.java  ..\src\main\java\skylee\exception\*.java   ..\src\main\java\skylee\storage\*.java  ..\src\main\java\skylee\parser\*.java  ..\src\main\java\skylee\commands\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/skylee/*.java ../src/main/java/skylee/task/*.java ../src/main/java/skylee/io/*.java  ../src/main/java/skylee/exception/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/skylee/*.java ../src/main/java/skylee/task/*.java ../src/main/java/skylee/ui/*.java  ../src/main/java/skylee/exception/*.java   ../src/main/java/skylee/storage/*.java  ../src/main/java/skylee/parser/*.java  ../src/main/java/skylee/commands/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
