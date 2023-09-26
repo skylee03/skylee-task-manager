5ee9002443bdea68539b7b9fc6364385fe04e15a
skylee03 <1178715749@qq.com>
2023-09-26 17:17:37 +0800

    Make find command case-insensitive

--- src/main/java/skylee/commands/FindCommand.java
+++ src/main/java/skylee/commands/FindCommand.java
@@ -36,10 +36,22 @@ public class FindCommand extends Command {
         messages.add(MESSAGE_FIND);
         for (int i = 0; i < tasks.size(); i++) {
             final Task task = tasks.get(i);
-            if (task.getDescription().contains(keyword)) {
+            if (containsIgnoreCase(task.getDescription(), keyword)) {
                 messages.add((i + 1) + "." + tasks.get(i));
             }
         }
         return messages.toArray(new String[0]);
+    }
+
+    /**
+     * Checks if <code>String</code> contains a search <code>String</code> irrespective of case.
+     *
+     * @param str           The <code>String</code> to check.
+     * @param searchStr     The <code>String</code> to find.
+     * @return              <code>true</code> if the <code>String</code> contains the search <code>String</code>
+     *                      irrespective of case or <code>false</code> if not
+     */
+    private static boolean containsIgnoreCase(String str, String searchStr) {
+        return str.toLowerCase().contains(searchStr.toLowerCase());
     }
 }
